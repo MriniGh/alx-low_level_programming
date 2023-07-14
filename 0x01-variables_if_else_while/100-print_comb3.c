c4974832f608c518fa9eafbd9fd29dd4694df288
MriniGh <tifakaymen@gmail.com>
2023-07-14 16:14:35 -0700

    task 100

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+/**
+ * main - Entry point of the program
+ *
+ * This program determines the 1st digit of a given number.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int i, j;
+
+	for (i = 0; i < 10; i++)
+	{
+		for (j = i + 1; j < 10; j++)
+		{
+			putchar('0' + i);
+			putchar('0' + j);
+			if (i != 8 || j != 9)
+			{
+				putchar(',');
+				putchar(' ');
+			}
+		}
+	}
+	putchar('\n');
+	return (0);
+}
