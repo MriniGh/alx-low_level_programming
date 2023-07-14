f437224c0821047dcd17fc6147aae8c4ca999469
MriniGh <tifakaymen@gmail.com>
2023-07-14 08:49:15 -0700

    tsak 5

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point of the program
+ *
+ * This program determines the 1st digit of a given number.
+ *
+ * Return: 0 on success
+ */
+int main(void)
+{
+	int a;
+
+	for (a = 0; a < 10 ; a++)
+	{
+		printf("%d", a);
+	}
+	printf("\n");
+	return (0);
+}
