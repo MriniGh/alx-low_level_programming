7f68d228ddc683adc3ceb8c5224c0508847d17aa
MriniGh <tifakaymen@gmail.com>
2023-07-18 07:54:42 -0700

    task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -10,16 +10,9 @@ int print_last_digit(int n)
 {
 	int last_digit;
 
-	if (n == INT_MIN)
-	{
-		last_digit = 8;
-	}
-	else
-	{
-		last_digit = n % 10;
-		if (last_digit < 0)
-			last_digit *= -1;
-		_putchar(last_digit + '0');
-	}
+	last_digit = n % 10;
+	if (last_digit < 0)
+		last_digit *= -1;
+	_putchar(last_digit + '0');
 	return (last_digit);
 }
