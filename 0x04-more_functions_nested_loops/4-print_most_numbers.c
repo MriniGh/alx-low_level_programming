0d6395f523dbdb3c867f40419e9e55311a45f8f4
MriniGh <tifakaymen@gmail.com>
2023-07-20 11:39:18 -0700

    task 4

--- 0x04-more_functions_nested_loops/4-print_most_numbers.c
+++ 0x04-more_functions_nested_loops/4-print_most_numbers.c
@@ -6,14 +6,12 @@
 void print_most_numbers(void)
 {
 	int i;
-	char digit;
 
-	for (i = 0; i <= 9; i++)
+	for (i = '0'; i <= '9'; i++)
 	{
-		if (i == 2 || i == 4)
+		if (i == '2' || i == '4')
 			i++;
-		digit = i + '0';
-		_putchar(digit);
+		_putchar(i);
 	}
 	_putchar('\n');
 }
